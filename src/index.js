eb5295262554915f4fd206e544c1908b2053cc08
saninoleg <96084949+saninoleg@users.noreply.github.com>
2022-02-27 01:39:20 +0300

    add: task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(avareigh, rubystine) {
+  for (let euan = 0; euan < rubystine.length;) {
+    let anyela = rubystine[euan].join("");
+    if (avareigh.includes(anyela)) {
+      avareigh = avareigh.replace(anyela, "");
+      euan = 0;
+    } else {
+      euan++;
+    }
+  }
+  ;
+  return !avareigh.length;
+};
